0abd6ee23b17518e452a413ff7b275910c46c688
Jan-H-Christensen <janx802y@easv365.dk>
2024-10-04 12:18:19 +0200

    🚀started on implementing user (added todos for me)

---
+++ UserService/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UserService;
+
+public class User
+{
+    public static void Main(string[] args)
+    {
+        //TOTO: implement user service logic her project is changed to a console app
+        Console.WriteLine("Hello, World!");
+    }
+}
